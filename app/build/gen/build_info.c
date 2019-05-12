e0171329e45fa0aa014cd42099b287a465351b0d
Ted Huntington <tedhuntington@gmail.com>
2019-05-12 16:54:37 -0700

    adding initial code

---
+++ app/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190512-225611";
+const char *build_timestamp = "2019-05-12T22:56:11Z";
+const char *build_version = "1.0";
---
+++ app/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20190512-225611/g986d287-master";
+const char *mg_build_timestamp = "2019-05-12T22:56:11Z";
+const char *mg_build_version = "986d287";
